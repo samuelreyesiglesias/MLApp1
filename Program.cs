4366d80d619fd9d0fc451e53c7680c5f82844870
samuelreyesiglesias <samuelreyesiglesias@outlook.com>
2024-04-29 18:14:04 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
